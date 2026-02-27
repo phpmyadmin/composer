e1ffe48b50e06616fa331b194ee43dd804c39632
Maurício Meneghini Fauth <mauricio@mfauth.net>
2026-02-27 16:58:22 -0300

    Add unit test for MultiTableQuery\QueryController
    Signed-off-by: Maurício Meneghini Fauth <mauricio@mfauth.net>

---
+++ tests/unit/Controllers/Database/MultiTableQuery/QueryControllerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpMyAdmin\Tests\Controllers\Database\MultiTableQuery;
+
+use Fig\Http\Message\StatusCodeInterface;
+use PhpMyAdmin\Bookmarks\BookmarkRepository;
+use PhpMyAdmin\Config;
+use PhpMyAdmin\ConfigStorage\Relation;
+use PhpMyAdmin\ConfigStorage\RelationCleanup;
+use PhpMyAdmin\Controllers\Database\MultiTableQuery\QueryController;
+use PhpMyAdmin\Current;
+use PhpMyAdmin\Dbal\DatabaseInterface;
+use PhpMyAdmin\Http\Factory\ServerRequestFactory;
+use PhpMyAdmin\Sql;
+use PhpMyAdmin\Template;
+use PhpMyAdmin\Tests\AbstractTestCase;
+use PhpMyAdmin\Tests\Stubs\DbiDummy;
+use PhpMyAdmin\Tests\Stubs\ResponseRenderer;
+use PhpMyAdmin\Transformations;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(QueryController::class)]
+final class QueryControllerTest extends AbstractTestCase
+{
+    public function testController(): void
+    {
+        Current::$database = 'test_db';
+        Current::$table = 'test_table';
+
+        $dbiDummy = $this->createDbiDummy();
+        $dbiDummy->addSelectDb('test_db');
+        // phpcs:disable Generic.Files.LineLength.TooLong
+        $dbiDummy->addResult(
+            'SELECT `test_table`.* FROM `test_table` LIMIT 0, 25',
+            [['1', 'abcd', '2011-01-20 02:00:02'], ['2', 'foo', '2010-01-20 02:00:02'], ['3', 'Abcd', '2012-01-20 02:00:02']],
+            ['id', 'name', 'datetimefield'],
+        );
+        $dbiDummy->addResult('SELECT 1 FROM information_schema.VIEWS WHERE TABLE_SCHEMA = \'test_db\' AND TABLE_NAME = \'test_table\'', []);
+        $dbiDummy->addResult('SELECT 1 FROM information_schema.VIEWS WHERE TABLE_SCHEMA = \'test_db\' AND TABLE_NAME = \'test_table\'', []);
+        $dbiDummy->addResult('SELECT 1 FROM information_schema.VIEWS WHERE TABLE_SCHEMA = \'test_db\' AND TABLE_NAME = \'test_table\'', []);
+        $dbiDummy->addResult('SELECT 1 FROM information_schema.VIEWS WHERE TABLE_SCHEMA = \'test_db\' AND TABLE_NAME = \'test_table\'', []);
+        $dbiDummy->addResult(
+            'SELECT *, `TABLE_SCHEMA` AS `Db`, `TABLE_NAME` AS `Name`, `TABLE_TYPE` AS `TABLE_TYPE`, `ENGINE` AS `Engine`, `ENGINE` AS `Type`, `VERSION` AS `Version`, `ROW_FORMAT` AS `Row_format`, `TABLE_ROWS` AS `Rows`, `AVG_ROW_LENGTH` AS `Avg_row_length`, `DATA_LENGTH` AS `Data_length`, `MAX_DATA_LENGTH` AS `Max_data_length`, `INDEX_LENGTH` AS `Index_length`, `DATA_FREE` AS `Data_free`, `AUTO_INCREMENT` AS `Auto_increment`, `CREATE_TIME` AS `Create_time`, `UPDATE_TIME` AS `Update_time`, `CHECK_TIME` AS `Check_time`, `TABLE_COLLATION` AS `Collation`, `CHECKSUM` AS `Checksum`, `CREATE_OPTIONS` AS `Create_options`, `TABLE_COMMENT` AS `Comment` FROM `information_schema`.`TABLES` t WHERE `TABLE_SCHEMA` COLLATE utf8_bin IN (\'test_db\') AND t.`TABLE_NAME` COLLATE utf8_bin = \'test_table\' ORDER BY Name ASC',
+            [['def', 'test_db', 'test_table', 'BASE TABLE', 'InnoDB', '10', 'Dynamic', '3', '5461', '16384', '0', '0', '0', '4', '2011-12-13 14:15:16', null, null, 'utf8mb4_general_ci', null, '', '', '0', 'N', 'test_db', 'test_table', 'BASE TABLE', 'InnoDB', 'InnoDB', '10', 'Dynamic', '3', '5461', '16384', '0', '0', '0', '4', '2011-12-13 14:15:16', null, null, 'utf8mb4_general_ci', null, '', '']],
+            ['TABLE_CATALOG', 'TABLE_SCHEMA', 'TABLE_NAME', 'TABLE_TYPE', 'ENGINE', 'VERSION', 'ROW_FORMAT', 'TABLE_ROWS', 'AVG_ROW_LENGTH', 'DATA_LENGTH', 'MAX_DATA_LENGTH', 'INDEX_LENGTH', 'DATA_FREE', 'AUTO_INCREMENT', 'CREATE_TIME', 'UPDATE_TIME', 'CHECK_TIME', 'TABLE_COLLATION', 'CHECKSUM', 'CREATE_OPTIONS', 'TABLE_COMMENT', 'MAX_INDEX_LENGTH', 'TEMPORARY', 'Db', 'Name', 'TABLE_TYPE', 'Engine', 'Type', 'Version', 'Row_format', 'Rows', 'Avg_row_length', 'Data_length', 'Max_data_length', 'Index_length', 'Data_free', 'Auto_increment', 'Create_time', 'Update_time', 'Check_time', 'Collation', 'Checksum', 'Create_options', 'Comment'],
+        );
+        // phpcs:enable
+
+        $request = ServerRequestFactory::create()->createServerRequest('POST', 'https://example.com/')
+            ->withQueryParams(['route' => '/database/multi-table-query/query'])
+            ->withParsedBody(['db' => 'test_db', 'sql_query' => 'SELECT `test_table`.* FROM `test_table`;']);
+
+        $response = ($this->getQueryController($dbiDummy))($request);
+
+        $dbiDummy->assertAllSelectsConsumed();
+        $dbiDummy->assertAllQueriesConsumed();
+        self::assertSame(StatusCodeInterface::STATUS_OK, $response->getStatusCode());
+        self::assertStringMatchesFormatFile(
+            __DIR__ . '/Fixtures/Query-testController.html',
+            (string) $response->getBody(),
+        );
+    }
+
+    private function getQueryController(DbiDummy $dbiDummy): QueryController
+    {
+        $config = Config::$instance = new Config();
+        $dbi = DatabaseInterface::$instance = $this->createDatabaseInterface($dbiDummy, $config);
+        $relation = new Relation($dbi, $config);
+        $responseRenderer = new ResponseRenderer();
+        $sql = new Sql(
+            $dbi,
+            $relation,
+            new RelationCleanup($dbi, $relation),
+            new Transformations($dbi, $relation),
+            new Template($config),
+            new BookmarkRepository($dbi, $relation),
+            $config,
+            $responseRenderer,
+        );
+
+        return new QueryController($responseRenderer, $sql);
+    }
+}
